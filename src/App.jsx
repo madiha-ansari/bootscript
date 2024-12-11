e012dcc31112d8d386464b8e46dc6e4ebfe5516c
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-12-11 13:08:27 +0500

    bootscript used in react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import Carousal from './Components/Carousal'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Carousal />
+    <Footer />
+    </>
+  )
+}
+
+export default App
